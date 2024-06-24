2a35c7a92d11f50b59880fa0a7946b9638974f15
chidoriii7 <muratore.danilo@yahoo.it>
2024-06-25 00:35:01 +0200

    php-oop-1: milestone 1 e 2

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+class Movie {
+
+
+    public $titolo;
+    public $durata;
+
+}
+
+$shutter_island = new Movie();
+$shutter_island->titolo = 'Shutter Island';
+$shutter_island->durata = '2h 18m';
+
+$fight_club = new Movie();
+$fight_club->titolo = 'Fight Club';
+$fight_club->durata = '2h 19m';
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php oop v1</title>
+</head>
+<body>
+    
+</body>
+</html>
